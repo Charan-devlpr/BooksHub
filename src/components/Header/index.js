4d250bcf487b27d5cd1df18c5efee9e61a794335
Charan-devlpr <117968667+Charan-devlpr@users.noreply.github.com>
2023-01-10 08:45:47 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,84 @@
 import Cookies from 'js-cookie'
+import {Component} from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi'
 import {Link, withRouter} from 'react-router-dom'
 import './index.css'
 
-const Header = props => {
-  const onClickedLogout = () => {
+class Header extends Component {
+  state = {homeTabStatus: true, bookShelfTabStatus: false}
+
+  onClickedLogout = () => {
     Cookies.remove('jwt_token')
-    const {history} = props
+    const {history} = this.props
     history.replace('/login')
   }
 
-  return (
-    <nav className="navbar-container">
-      <Link to="/">
-        <div className="container">
-          <img
-            src="https://res.cloudinary.com/dxqcmp4il/image/upload/v1672811169/Group_7730_2x_1_xsumg4.png"
-            alt="website logo"
-            className="website-logo"
-          />
-          <h1 className="bookHub-text">ookHub</h1>
-        </div>
-      </Link>
-      <div className="container">
-        <ul className="unOrder-items">
-          <Link to="/">
-            <li className="list-items">Home</li>
+  onClickedHomeTab = () => {
+    this.setState({homeTabStatus: true, bookShelfTabStatus: false})
+  }
+
+  onClickedBookShelfTab = () => {
+    this.setState({bookShelfTabStatus: true, homeTabStatus: false})
+  }
+
+  render() {
+    const {homeTabStatus, bookShelfTabStatus} = this.state
+    const homeTabColor = homeTabStatus ? 'active-nav-item' : 'nav-item'
+    const bookShelfColor = bookShelfTabStatus ? 'active-nav-item' : 'nav-item'
+    return (
+      <div>
+        <nav className="navbar-container">
+          <Link to="/" className="link-item">
+            <div className="container">
+              <img
+                src="https://res.cloudinary.com/dxqcmp4il/image/upload/v1672811169/Group_7730_2x_1_xsumg4.png"
+                alt="website logo"
+                className="website-logo"
+              />
+              <h1 className="bookHub-text">ookHub</h1>
+            </div>
           </Link>
-          <Link to="/bookshelves">
-            <li className="list-items">Bookshelves</li>
+          <div className="container">
+            <ul className="unOrder-items">
+              <Link to="/" className="link-item">
+                <li className={homeTabColor} onClick={this.onClickedHomeTab}>
+                  Home
+                </li>
+              </Link>
+              <Link to="/bookshelves" className="link-item">
+                <li
+                  className={bookShelfColor}
+                  onClick={this.onClickedBookShelfTab}
+                >
+                  Bookshelves
+                </li>
+              </Link>
+            </ul>
+            <button
+              className="logout-btn"
+              type="button"
+              onClick={this.onClickedLogout}
+            >
+              Logout
+            </button>
+          </div>
+        </nav>
+        <nav className="small-navbar-container">
+          <Link to="/" className="link-item">
+            <div className="container">
+              <img
+                src="https://res.cloudinary.com/dxqcmp4il/image/upload/v1672811169/Group_7730_2x_1_xsumg4.png"
+                alt="website logo"
+                className="website-logo"
+              />
+              <h1 className="bookHub-text">ookHub</h1>
+            </div>
           </Link>
-        </ul>
-        <button className="logout-btn" type="button" onClick={onClickedLogout}>
-          Logout
-        </button>
+          <GiHamburgerMenu className="hamburger" />
+        </nav>
       </div>
-    </nav>
-  )
+    )
+  }
 }
 
 export default withRouter(Header)
