15216f6ca5a91d0f723147b5f7b38aa709234ee8
Charan-devlpr <117968667+Charan-devlpr@users.noreply.github.com>
2023-01-10 08:50:37 +0530

    Update index.js

--- src/components/BookShelvesList/index.js
+++ src/components/BookShelvesList/index.js
@@ -1,21 +1,35 @@
+import {Component} from 'react'
 import './index.css'
 
-const BookShelvesList = props => {
-  const {bookshelvesListDetails, updateBookshelf} = props
-  const {label, value} = bookshelvesListDetails
-  const onClickedTab = () => {
-    updateBookshelf(value, label)
+class BookShelvesList extends Component {
+  render() {
+    const {bookshelvesListDetails, updateBookshelf} = this.props
+    const {label, value} = bookshelvesListDetails
+    const onClickedTab = () => {
+      updateBookshelf(value, label)
+    }
+
+    return (
+      <li>
+        <button
+          type="button"
+          className="bookShelves"
+          value={value}
+          onClick={onClickedTab}
+        >
+          {label}
+        </button>
+        <button
+          type="button"
+          className="small-bookShelves"
+          value={value}
+          onClick={onClickedTab}
+        >
+          {label}
+        </button>
+      </li>
+    )
   }
-  return (
-    <button
-      type="button"
-      className="bookShelves"
-      value={value}
-      onClick={onClickedTab}
-    >
-      {label}
-    </button>
-  )
 }
 
 export default BookShelvesList
