9c1ba5bb51311aea664fd89c98bb1687c442015f
Charan-devlpr <117968667+Charan-devlpr@users.noreply.github.com>
2023-01-10 08:48:30 +0530

    Update index.js

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -7,26 +7,24 @@ const Books = props => {
   const {eachBookDetails} = props
   const {id, coverPic, title, authorName, rating, readStatus} = eachBookDetails
   return (
-    <Link to={`/books/${id}`}>
-      <li>
-        <div className="book-container">
-          <img src={coverPic} className="cover-pic" alt={title} />
-          <div>
-            <h1 className="book-heading">{title}</h1>
-            <p className="author-name">{authorName}</p>
-            <div className="container">
-              <p className="avg-rating">Avg Rating</p>
-              <BsFillStarFill className="star-icon" />
-              <p className="rating">{rating}</p>
-            </div>
-            <div className="container">
-              <p className="status">Status: </p>
-              <p className="read-status">{readStatus}</p>
-            </div>
+    <li className="book-container">
+      <Link to={`/books/${id}`} className="link-item">
+        <img src={coverPic} className="cover-pic" alt={title} />
+        <div>
+          <h1 className="book-heading">{title}</h1>
+          <p className="author-name">{authorName}</p>
+          <div className="container">
+            <p className="avg-rating">Avg Rating</p>
+            <BsFillStarFill className="star-icon" />
+            <p className="rating">{rating}</p>
+          </div>
+          <div className="container">
+            <p className="status">Status: </p>
+            <p className="read-status">{readStatus}</p>
           </div>
         </div>
-      </li>
-    </Link>
+      </Link>
+    </li>
   )
 }
 export default Books
